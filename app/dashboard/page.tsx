e4d10534b15eb319e9bcc68eb9c717f3595a0d90
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-12-24 17:25:14 +0500

    The Dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image';
+
+const page = () => {
+  return (
+    <>
+      hey my name is shahzaib durrani
+    </>
+  )
+}
+
+export default page
